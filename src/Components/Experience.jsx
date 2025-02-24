9d4dd2ccbe804d3031673b36c64c1fa9006b3ef6
Parusharaut <parusharaut@gmail.com>
2025-02-24 13:36:00 -0600

    changed image src

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./Experience.module.css";
+import { getImageUrl } from "../utils";
 
 export const Experience = () => {
   return (
@@ -13,14 +14,14 @@ export const Experience = () => {
         <div className={styles.skillsSection}>
           <h3 className={styles.sectionTitle}>Skills</h3>
           <div className={styles.iconsGrid}>
-            <div className={styles.skillIcon}><img src="/assets/java.png" alt="Java" /><span>Java</span></div>
-            <div className={styles.skillIcon}><img src="/assets/python.png" alt="Python" /><span>Python</span></div>
-            <div className={styles.skillIcon}><img src="/assets/cpp.png" alt="C++" /><span>C++</span></div>
-            <div className={styles.skillIcon}><img src="/assets/sql.png" alt="SQL" /><span>SQL</span></div>
-            <div className={styles.skillIcon}><img src="/assets/react.png" alt="React" /><span>React</span></div>
-            <div className={styles.skillIcon}><img src="/assets/node.png" alt="Node.js" /><span>Node.js</span></div>
-            <div className={styles.skillIcon}><img src="/assets/git.png" alt="Git" /><span>Git</span></div>
-            <div className={styles.skillIcon}><img src="/assets/kali.png" alt="Kali Linux" /><span>Kali Linux</span></div>
+            <div className={styles.skillIcon}><img src={getImageUrl("java.png")} alt="Java" /><span>Java</span></div>
+            <div className={styles.skillIcon}><img src={getImageUrl("python.png")} alt="Python" /><span>Python</span></div>
+            <div className={styles.skillIcon}><img src={getImageUrl("cpp.png")} alt="C++" /><span>C++</span></div>
+            <div className={styles.skillIcon}><img src={getImageUrl("sql.png")} alt="SQL" /><span>SQL</span></div>
+            <div className={styles.skillIcon}><img src={getImageUrl("react.png")} alt="React" /><span>React</span></div>
+            <div className={styles.skillIcon}><img src={getImageUrl("node.png")} alt="Node.js" /><span>Node.js</span></div>
+            <div className={styles.skillIcon}><img src={getImageUrl("git.png")} alt="Git" /><span>Git</span></div>
+            <div className={styles.skillIcon}><img src={getImageUrl("kali.png")} alt="Kali Linux" /><span>Kali Linux</span></div>
           </div>
         </div>
 
@@ -31,7 +32,7 @@ export const Experience = () => {
 
             {/* Experience 1 */}
             <div className={styles.experienceCard}>
-              <img src="/assets/ulm.png" alt="University" className={styles.companyLogo} />
+              <img src={getImageUrl("ulm.png")} alt="University" className={styles.companyLogo} />
               <div className={styles.experienceContent}>
                 <h3>IT Technician</h3>
                 <p><em>University of Louisiana Monroe | May 2024 – Present</em></p>
@@ -44,7 +45,7 @@ export const Experience = () => {
 
             {/* Experience 2 */}
             <div className={styles.experienceCard}>
-              <img src="/assets/ulm.png" alt="University" className={styles.companyLogo} />
+              <img src={getImageUrl("ulm.png")} alt="University" className={styles.companyLogo} />
               <div className={styles.experienceContent}>
                 <h3>IT Call Center</h3>
                 <p><em>University of Louisiana Monroe | April 2022 – May 2024</em></p>
@@ -57,7 +58,7 @@ export const Experience = () => {
 
             {/* Experience 3 */}
             <div className={styles.experienceCard}>
-              <img src="/assets/cleco.png" alt="CLECO LLC" className={styles.companyLogo} />
+              <img src={getImageUrl("cleco.png")} alt="CLECO LLC" className={styles.companyLogo} />
               <div className={styles.experienceContent}>
                 <h3>Intern – Network and Telecommunications</h3>
                 <p><em>CLECO LLC | May 2023 – August 2023</em></p>
